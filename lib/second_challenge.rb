030802c3c9d85c52e72ce9999f92afc5b788acf8
Jun Liang <illuminatemynight@gmail.com>
2019-06-23 01:34:33 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,17 +1,18 @@
 def second_challenge
   epic_tragedy = {
     :montague => {
-      :patriarch => {},#your key/value pairs here!
+      :patriarch => {},
       :matriarch => {},
       :hero => {},
       :hero_friends => []
-      },
+    },
     :capulet => {
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends => []#your key/value pairs here!
-      }}
+      :hero_friends => []
+    }
+}
 
 
 end
